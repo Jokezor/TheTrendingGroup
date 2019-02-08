790b9eb71fd84bc0b0ef27f8605268aa1e8e1a9b
Anton Lindqvist <antonlindqvist@client218-203.wireless.umu.se>
2019-02-08 14:20:26 +0100

    hejhej

--- Period_to_timeframe.py
+++ Period_to_timeframe.py
@@ -3,42 +3,53 @@
     which will be used by Google Trends API.
 
     Input: period_to_look (string)
-    Output: list of date formats (string)
+    Output: date format (string)
 '''
 
 from datetime import date
+from datetime import timedelta
+
 import Time_funcs
 
 def Period_to_timeframe(period_to_look):
     today = date.today()
-    timeframes = []
 
     if (period_to_look == "Year"):
         last_month = Time_funcs.subtract_one_month(today)
 
         dates = str(last_month) + " " + str(today)
-        timeframes.append(dates)
+        timeframe=dates
 
         for i in range(0,11):
             current_month = last_month
             last_month = Time_funcs.subtract_one_month(current_month)
 
             dates = str(last_month) + " " + str(current_month)
-            timeframes.append(dates)
+        dates = str(last_month) + " " + str(today)
+        print(dates)
+        timeframe=dates
 
-
-
-    '''
     if (period_to_look == "Month"):
 
+        last_month = Time_funcs.subtract_one_month(today)
+        dates = str(last_month) + " " + str(today)
+
+        print(dates)
+        timeframe=dates
+
     if (period_to_look == "Week"):
+        last_week = date.today() - timedelta(days=7)
+        timeframe = str(last_week) + " " + str(today)
+        print(timeframe)
 
     if (period_to_look == "Day"):
-    '''
+        last_day = date.today() - timedelta(days=1)
+        timeframe = str(last_day) + " " + str(today)
+        print(timeframe)
 
-    print(timeframes)
+
 # Test to subtract one Month
 #today = date.today()
 #print(Time_funcs.subtract_one_month(today))
-period_to_look = "Year"
+period_to_look = "Day"
 Period_to_timeframe(period_to_look)
