d6cea93bcf04b95c694de134eb8d8cf129f1d773
Anton Lindqvist <antonlindqvist@client218-183.wireless.umu.se>
2019-02-18 14:15:48 +0100

    blakalfjl

---
+++ Correlation.py
@@ -0,0 +1,28 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def Get_correlation(Trend_data,Stock_data):
+
+    Company = 'AAPLE'
+
+    td = Trend_data['Apple Inc.']
+    sd = Stock_data['NEON']['close']
+
+    '''
+    print(sd)
+    print(len(sd))
+    print(len(td))
+    '''
+
+    # Min-max normalization
+    ntd = (td - min(td))/(max(td)-min(td))
+    nsd = (sd - min(sd))/(max(sd)-min(sd))
+
+
+    # Create figure and plot both normalized data-set in the same graph.
+    plt.plot(ntd,label='Trend Data')
+    plt.plot(nsd,label='Stock Data')
+    plt.title("Data Correlation")
+    plt.legend()
+    plt.show()
