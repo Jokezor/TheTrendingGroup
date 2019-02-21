8116b7ffc8f62a57b73804f4c0f89ab6b0407f7a
Jokezor <joakimolovsson@hotmail.com>
2019-02-21 11:03:57 +0100

    Fixed Stock_data.py for when market is closed

--- Company_to_ticker.py
+++ Company_to_ticker.py
@@ -9,14 +9,20 @@ import pandas as pd
 
 def Company_to_ticker(Company):
 
+
+    # Nasdaq
     df = pd.read_csv('companylist.csv')
 
     tmpTickers = []
-
     for i in range(len(Company)):
         test = df[df.Name==Company[i]]
 
         if not (test.empty):
             tmpTickers.append(test.Symbol.values[0])
 
-    return tmpTickers
+    # Return the tickers if the Company was listed in Nasdaq
+    if  len(tmpTickers)!=0:
+        # Need to return also a list of all the nasdaq holidays
+        # 'Month-Day'
+        Nasdaq_closed = ['01-01','01-21','02-18','04-19','05-27','07-04','09-02','11-28','12-25']
+        return tmpTickers, Nasdaq_closed
--- Correlation.py
+++ Correlation.py
@@ -7,7 +7,7 @@ def Get_correlation(Trend_data,Stock_data):
     Company = 'AAPL'
 
     td = Trend_data['Apple Inc.']
-    sd = Stock_data['NEON']['close']
+    sd = Stock_data['AAPL']['close']
 
 
     '''
@@ -15,11 +15,14 @@ def Get_correlation(Trend_data,Stock_data):
     print(len(sd))
     print(len(td))
     '''
+    print(len(td))
+    print(len(sd))
 
     # Min-max normalization
     ntd = (td - min(td))/(max(td)-min(td))
     nsd = (sd - min(sd))/(max(sd)-min(sd))
-    print(Trend_data['Date'])
+    #print (Trend_data['Date'])
+    #print (sd)
 
 
     # Create figure and plot both normalized data-set in the same graph.
